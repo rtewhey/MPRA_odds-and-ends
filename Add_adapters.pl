03d137e88fcca2fd50769aba682e589dc38d3f7c
Ryan Tewhey <ryan.tewhey@jax.org>
2018-08-01 18:15:59 -0400

    -B flag
    added -B flag to accomidate bashing file format.

--- Add_adapters.pl
+++ Add_adapters.pl
@@ -8,6 +8,18 @@
 use strict;
 use warnings;
 use POSIX;
+use Getopt::Std;
+
+
+my %options=();
+getopts('B', \%options);
+
+#####
+#
+#-B = Use flag -B if BASH format (ID Oligo)
+#
+#####
+
 
 my $ALLELES = $ARGV[0];
 
@@ -34,20 +46,68 @@ my $oligo;
 my $oligo_rc;
 my $enhancer_rc;
 
+my $tmp_seq;
+my $a_ct;
+my $c_ct;
+my $g_ct;
+my $t_ct;
+my $gc;
 
 #######################
 ###Load Allele File
 #######################
 
+if($options{B}) {
+open (ALLELES, $ALLELES) or die "couldn't open $ALLELES\n";
+while (<ALLELES>)
+{
+    $_ =~ s/[\n\r]//g;
+    @inline = split(/\t/);
+	$length = length($inline[1]);
+	$oligo = $l_adapter.$inline[1].$r_adapter;
+	
+	die "Enhancer is to large to fit an oligo of $oligo_length bp\n" if(length($oligo) > $oligo_length);
+	
+	if(length($oligo) < $oligo_length)
+		{
+		$residual = $oligo_length - length($oligo);
+		
+		$left_add = floor(($residual)/2);
+		$right_add = ceil(($residual)/2);
+		
+		if($left_add < 1)
+			{
+			#$oligo_rc=$oligo_rc.substr($r_pad,0,$right_add) ;
+			$oligo=$oligo.substr($r_pad,0,$right_add);
+			}
+		else
+			{
+			#$oligo_rc=substr($l_pad,-$left_add).$oligo_rc.substr($r_pad,0,$right_add) ;
+			$oligo=substr($l_pad,-$left_add).$oligo.substr($r_pad,0,$right_add);	
+			}
+		}
+		
+	$tmp_seq = $inline[1];
+	$a_ct=0;$c_ct=0;$g_ct=0;$t_ct=0;
+	$a_ct = uc($tmp_seq) =~ tr/A//;
+	$c_ct = uc($tmp_seq) =~ tr/C//;
+	$g_ct = uc($tmp_seq) =~ tr/G//;
+	$t_ct = uc($tmp_seq) =~ tr/T//;
+	$gc	= sprintf("%.1f",100*(($c_ct+$g_ct)/($a_ct+$c_ct+$g_ct+$t_ct)));	
+				
+	print join("\t",$inline[0],"NA","NA","NA",$gc,length($inline[1]),length($oligo),uc($oligo))."\n";
+	#print join("\t",$inline[0]."_rc",$inline[1],$inline[2],$inline[3],$inline[4],$inline[5],length($oligo_rc),$oligo_rc)."\n";	
+}
+close ALLELES;
+}
+
+else {
 open (ALLELES, $ALLELES) or die "couldn't open $ALLELES\n";
 while (<ALLELES>)
 {
     $_ =~ s/[\n\r]//g;
     @inline = split(/\t/);
 	$length = length($inline[13]);
-	#$enhancer_rc = reverse($inline[13]);
-    #$enhancer_rc =~ tr/ABCDGHMNRSTUVWXYabcdghmnrstuvwxy/TVGHCDKNYSAABWXRtvghcdknysaabwxr/;
-	#$oligo_rc = $l_adapter.$enhancer_rc.$r_adapter;
 	$oligo = $l_adapter.$inline[13].$r_adapter;
 	
 	die "Enhancer is to large to fit an oligo of $oligo_length bp\n" if(length($oligo) > $oligo_length);
@@ -71,10 +131,7 @@ while (<ALLELES>)
 			}
 		}
 	print join("\t",$inline[0],$inline[1],$inline[2],$inline[3],$inline[4],$inline[5],length($oligo),$oligo)."\n";
-	#print join("\t",$inline[0]."_rc",$inline[1],$inline[2],$inline[3],$inline[4],$inline[5],length($oligo_rc),$oligo_rc)."\n";
-
-	
-	
-	
+	#print join("\t",$inline[0]."_rc",$inline[1],$inline[2],$inline[3],$inline[4],$inline[5],length($oligo_rc),$oligo_rc)."\n";	
 }
 close ALLELES;
+}
